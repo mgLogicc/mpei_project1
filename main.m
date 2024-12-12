e9f0c477552c26ad98c27718a50ce738e94f3d50
gRibau <goncalob.ribau@gmail.com>
2024-12-12 23:41:15 +0000

    Confirmação Bloom filter works like a charm (dá uns poucos falsos positivos mas não dá falsos negativos )

--- main.m
+++ main.m
@@ -1,7 +1,34 @@
 %% função main do projeto
 
-%teste
-test_link = 'proudemocrat.com';
+%teste (links que pertencem)
+% test_links = {'fort-russ.com';
+%     'prisonplanet.com';
+%     'truthfeed.com';
+%     'themindunleashed.com';
+%     'usuncut.com';
+%     'newstarget.com';
+%     'counterpunch.org';
+%     'jewsnews.co.il';
+%     'educateinspirechange.org';
+%     'clickhole.com'
+% };
+
+% teste (links que não pertencem)
+test_links = {'www.sparkleverse.net';
+    'quantumoodles.com';
+    'pixelpenguin.io';
+    'frogtastic.fun';
+    'gadgetgalaxy.org';
+    'cupcakery.zone';
+    'cyberkoala.co';
+    'bluemangoes.tech';
+    'whiskerwhale.xyz';
+    'nightowlapps.dev'
+};
+
+for l = 1:length(test_links) % for de teste
+test_link = test_links{l}; % link em teste
+
 
 %% verificação inicial usando Bloom Filter
 
@@ -18,7 +45,7 @@ data = readtable('bf_data_fake.csv'); % links processados
 % inserção de elementos no bloom filter
 for i = 1:height(data)
     bloom_filter = insert(data.site_url{i}, bloom_filter, k);
-    disp(data.site_url{i})
+    % disp(data.site_url{i}); % mostrar o que foi inserido
 end
 
 % verificar se um link fornecido pertence à base de dados
@@ -27,3 +54,7 @@ ismember = verify(bloom_filter, test_link, k)
 if ismember
     % neste caso passa para o Classificador de Naive Bayes
 end
+
+
+end % end do for de teste
+fprintf("end of test")
