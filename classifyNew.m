9b6eee099c902d0131764db700e5f953ee6edc42
Chaliça <44166339+mgLogicc@users.noreply.github.com>
2024-12-16 22:15:20 +0000

    NAIVE BAYES A FUNCIONAR

---
+++ classifyNew.m
@@ -0,0 +1,36 @@
+function class = classifyNew(new, uniqueWords, probWordReal, probWordFake, probFake, probReal)
+    %   This function classifies a new as fake or real using Naive-Bayes  
+    %       
+    %   Inputs:
+    %   - new : title and content of a new (as one char vector) to be tested
+    %   - uniqueWords : vector with the unique words of the data
+    %   - probWordReal : vector with probs of every word in vocab appear in a real new 
+    %   - probWordFake : vector with probs of every word in vocab appear in a fake new
+    %   - probFake : prior probability of a news being fake
+    %   - probReal : prior probability of a news being real
+    %
+    %   Output:
+    %   - class : class given to the new (fake or real)
+    disp(new)
+    words = split(new);                 % Extract current new's words
+    
+    % We are going to work with logaritmic values to avoid underflow
+    logProbFake = log(probFake);        
+    logProbReal = log(probReal);
+
+    % Get index in the vocabulary of every word in the new
+    [found, vocabIdx] = ismember(words, uniqueWords);
+    
+    % Keep only the found indexes (words in the vocabulary)
+    validIdx =  vocabIdx(found);
+        
+    % Sum log probabilities for valid words
+    logProbFake = logProbFake + sum(log(probWordFake(validIdx)));
+    logProbReal = logProbReal + sum(log(probWordReal(validIdx)));
+   
+    if logProbFake > logProbReal
+        class = 'Fake';
+    else
+        class = 'Real';
+    end
+end
