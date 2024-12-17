0307236056bf1eeb6999d968d50f7f184ab2d0a9
Chaliça <44166339+mgLogicc@users.noreply.github.com>
2024-12-17 23:40:17 +0000

    Tudo a funcionar

--- mainTestes.m
+++ mainTestes.m
@@ -1,4 +1,70 @@
+%% Bloom Filters
 
+% testes
+test_links = belong_links(); % links que pertencem
+%test_links = dontbelong_links(); % links que não pertencem
+
+ones1 = 0; % nº de positivos
+zeros0 = 0; % nº de negativos
+
+% array para armazenar resultado 
+results = {};
+
+for l = 1:length(test_links) % for de teste
+    test_link = test_links{l}; % link em teste
+
+    % verificação inicial usando Bloom Filter
+    
+    linkscsv = 'bf_data_fake.csv'; % ficheiro CSV com os links
+    
+    data = readtable(linkscsv); % links processados
+    urls=unique(data.site_url);
+    clear data
+    
+    % parâmetros iniciais
+    m = length(urls);
+    p = 0.001; % probabilidade de falsos positivos desejada (1%)
+    n = ceil(-1 * (m * log(p))/(log(2))^2); % tamanho do vetor 
+    k = ceil((n/m)*log(2)); % nº de funções de dispersão
+    
+    % inicialização do bloom filter
+    bloom_filter = initialize(n);
+    
+    % inserção de elementos no bloom filter
+    for i = 1:length(urls)
+        bloom_filter = insert(urls{i}, bloom_filter, k);
+        % disp(data.site_url{i}); % mostrar o que foi inseridoc
+    end
+    
+    % verificar se um link fornecido pertence à base de dados
+    ismember = verify(bloom_filter, test_link, k);
+    %% Teste Bloom Filter
+    
+    %% testar os nº de falsos positivos
+    if ismember == 1
+        ones1 = ones1 + 1;
+        fprintf('A notícia do site "%s" pode ser fake news!\n', test_link)
+    else
+        zeros0 = zeros0 + 1;
+        fprintf('A notícia do site "%s" não está registada como possível fake news!\n', test_link)
+    end
+    
+    % Adicionando os resultados ao array
+    results{1, l} = test_link;  % Adiciona o link na primeira linha
+    results{2, l} = ismember;
+    
+    end % end do for de teste
+    
+    % prints de informação
+    fprintf('Logical Ones (1): %d\n', ones1)
+    fprintf('Logical Zeros (0): %d\n', zeros0)
+    
+    % if statement para mostrar os falsos positivos (só existem para o segundo
+    % array, já que o primeiro é de links que pertencem e é suposto todos darem
+    % 1)
+    if length(test_links) == 200
+        fprintf('False Positives: %.1f%%\n', (ones1*100)/length(test_links))
+end
 
 %% Naive Bayes
 % adição no matlab das paths dos diretórios relevantes ao programa
@@ -8,8 +74,8 @@ addpath('MinHash')
 addpath('Data')
 addpath('Files')
 
-file_name1 = 'Data/processedTrue.csv';                   % file name of the real news vocabulary
-file_name2 = 'Data/processedFake.csv';                   % file name of the fake news vocabulary
+file_name1 = 'Data/processedTrue.csv';              % file name of the real news vocabulary
+file_name2 = 'Data/processedFake.csv';              % file name of the fake news vocabulary
 
 % Extract data
 realNews  = readcell(file_name1);                   % Cell array - each line is a real new
@@ -33,7 +99,7 @@ R = randi(p, numHash,5);        % Matrix of random number for each hash function
 % Separate data set to have train and test data
 fakeTrainSize = round(0.8 * numFakeNews);           % Set 80% of the fake news data for train
 fakeTrainNews = fakeNews(1:fakeTrainSize);          % Extract the 80% from the data for train
-fakeTestNews = fakeNews(fakeTrainSize + 1 : end);  % Extract the last 20% for the tests
+fakeTestNews = fakeNews(fakeTrainSize + 1 : end);   % Extract the last 20% for the tests
 
 fakeTrainNews = fakeTrainNews(cellfun(@(x) ischar(x) || isstring(x), fakeTrainNews));
 fakeTrainSize = length(fakeTrainNews);
@@ -152,7 +218,7 @@ hf = 300;                   % Number of hash functions
 p = 1e9 + 7;                % High prime number
 R = randi(p, hf,5);         % Matrix of random number for each hash function
 
-new1 = testNews{1};
+new1 = 'trump abortion meant women punish every time donald trump tries walk back punishment comments manages become even offensive recent new york times interview reporter robert trump made yet another pathetic attempt clarify disgusting comments made interview msnbc chris matthews stated women get form punishment seeking trump originally tried correct comments shortly saying doctors perform illegal abortions face criminal charges still stance trump made another attempt defend comments trump mean punishment women like saying women punish want people think terms prison walked turns every time trump walks comments receiving backlash wants damage control really sorry draper investigated trump claims meant women discovered presumptive republican nominee lying draper reported trump initially saw nothing wrong remark refused walk back giving numerous media outlets swarmed trump tried soften blow another statement said exact two contradictory statements released approximately hour apart nothing new constantly contradicting watch trump original punishment comments see trump claim misinterpretation complete guy clearly hates major threat safety future gets white prove crazy trump times trump insisted would better women issues democratic front runner hillary clinton everybody else general election think good issue trump still remains completely oblivious almost three quarters american women hate shaming telling punish abortion going image via eduardo munoz alvarez getty images';
 shingles1 = generateShingles(new1, k);
 signature1 = minhashSignatures(shingles1, hf, p, R);
 
