32d4ded9735bef7435ce575c68602cfed1bdb1aa
Chaliça <44166339+mgLogicc@users.noreply.github.com>
2024-12-17 18:33:31 +0000

    App quase a funcionar

--- app.m
+++ app.m
@@ -2,8 +2,8 @@
 
 % adição no matlab das paths dos diretórios relevantes ao programa
 addpath('BloomFilter')
-addpath('NaiveBayes')
-addpath('MinHash')
+%addpath('NaiveBayes')
+%addpath('MinHash')
 addpath('Data')
 
 %% Parâmetros iniciais do Bloom Filter
@@ -62,7 +62,7 @@ if ismember == 1
     fprintf("Pressione Ctrl + C para saír.\n")
 else
     fprintf("O URL fornecido ('%s') não está marcado como fake news e terá de ser melhor avaliado.\n", link_given)
-   
+    clear all;
     %% Parâmetros Iniciais do Classificador de Naive Bayes
     
     prompt = 'Insira o título da notícia: ';
@@ -74,10 +74,22 @@ else
     full_news = sprintf('%s %s', news_title, news_body);
     disp(full_news);
 
-    % load de ficheiros pré definidos
-    % load('trainFake.mat')
-    % load('trainReal.mat')
-    % load('uniqueWords.mat')
+    %file_name1 = 'processedTrue.csv';                   % file name of the real news vocabulary
+    %file_name2 = 'processedFake.csv';                   % file name of the fake news vocabulary
+    
+    % Extract data
+    %realNews  = readcell(file_name1);                   % Cell array - each line is a real new
+    %fakeNews = readcell(file_name2);                    % Cell array - each line is a fake new
+    
+    %numFakeNews = length(fakeNews);                     % Total number of fake news
+    %numRealNews = length(realNews);                     % Total number of real news
+
+    %k = 5;                          % Size of shingles
+    %numHash = 300;                  % Number of hash functions
+    %p = 1e9 + 7;                    % High prime number
+    %R = randi(p, numHash,5);        % Matrix of random number for each hash function
+    
+%% Naive-Bayes Classifier Training
 
     file_name1 = 'processedTrue.csv';                   % file name of the real news vocabulary
     file_name2 = 'processedFake.csv';                   % file name of the fake news vocabulary
@@ -87,46 +99,79 @@ else
     fakeNews = readcell(file_name2);                    % Cell array - each line is a fake new
     
     numFakeNews = length(fakeNews);                     % Total number of fake news
-    numRealNews = length(realNews);                    % Total number of real news
+    numRealNews = length(realNews);                     % Total number of real news
     
-%% Naive-Bayes Classifier Training
-
+    % Keep only the first half
+    realNews = realNews(1:floor(numRealNews/3), :);
+    fakeNews = fakeNews(1:floor(numFakeNews/3), :);
+    
+    numFakeNews = length(fakeNews);                     % Total number of fake news
+    numRealNews = length(realNews);                     % Total number of real news
+    
+    %k = 5;                          % Size of shingles
+    %numHash = 300;                  % Number of hash functions
+    %p = 1e9 + 7;                    % High prime number
+    %R = randi(p, numHash,5);        % Matrix of random number for each hash function
+    
     % Separate data set to have train and test data
     fakeTrainSize = round(0.8 * numFakeNews);           % Set 80% of the fake news data for train
     fakeTrainNews = fakeNews(1:fakeTrainSize);          % Extract the 80% from the data for train
-    fakeTestNews = fakeNews(fakeTrainSize + 1 : end);  % Extract the last 20% for the tests
     
+    fakeTrainNews = fakeTrainNews(cellfun(@(x) ischar(x) || isstring(x), fakeTrainNews));
+    fakeTrainSize = length(fakeTrainNews);
+    
+    %fakeSignatureMatrix = zeros(numHash, fakeTrainSize); % Initialize the matrix
+    %for i = 1:fakeTrainSize
+    %    shingles = generateShingles(fakeTrainNews{i}, k); 
+    %    signature = minhashSignatures(shingles, numHash, p, R); 
+    %    
+    %    fakeSignatureMatrix(:, i) = signature; 
+    %end
+    load('fakeSignatureMatrix.mat')
+
     realTrainSize = round(0.8 * numRealNews);           % Same process as before, but for the real news data
     realTrainNews = realNews(1:realTrainSize);
-    realTestNews = realNews(realTrainSize + 1 : end);
     
+    realTrainNews = realTrainNews(cellfun(@(x) ischar(x) || isstring(x), realTrainNews));
+    realTrainSize = length(realTrainNews);
+    
+    %realSignatureMatrix = zeros(numHash,realTrainSize);
+    %for i = 1:realTrainSize
+    %    shingles = generateShingles(realTrainNews{i}, k);
+    %    signature = minhashSignatures(shingles, numHash, p, R); 
+    %    
+    %    realSignatureMatrix(:,i) = signature;
+    %end
+    load('realSignatureMatrix.mat')
+
     trainNews = [realTrainNews; fakeTrainNews];         % Build the train array with the firsts being the real news
-    testNews = [realTestNews; fakeTestNews];            % Build the test array
     
     % cellfun will apply a function to each element of a cell array (x)
     % ischar(x) will check if its a char array and isstring(x) will check if
     % its a string. This will create a logical array that meet this conditions
     % Then we use logical indexing to store only valid elements
-    trainNews = trainNews(cellfun(@(x) ischar(x) || isstring(x), trainNews));
+    %trainNews = trainNews(cellfun(@(x) ischar(x) || isstring(x), trainNews));
     
     % Get unique words
     %uniqueWords = extractUniqueWords(trainNews); 
-    
+    load('uniqueWords.mat')
     % Get word occurences 
     %occurenceMatrix = getWordOccurences(trainNews, uniqueWords);
-        
+    load('occurenceMatrix.mat')
     % Probability calculations for the Naive Bayes implementation
-    
+ 
     possibleCases = length(trainNews);                  % Possibles cases
     
     % P('Fake'):
     probFake = fakeTrainSize / possibleCases;           % Probability of a new being fake
+    
     % P('Real'):
     probReal = realTrainSize / possibleCases;           % Probability of a new being real
     
     % P(words | 'Real'):
     %trainReal = occurenceMatrix(1:realTrainSize,:);     % Occurence matrix of real news
-    
+    load('trainReal.mat')
+
     wordCountReal = sum(trainReal) + 1;                 % Ocurrences of each word in real news (numerator)
     totalWordsReal = sum(wordCountReal);                % Number of total words (denominator)
     
@@ -134,13 +179,21 @@ else
     
     % P(words | 'Fake'):
     %trainFake = occurenceMatrix(realTrainSize+1:end,:); % Occurence matrix of fake news
+    load('trainFake.mat')
     
     wordCountFake = sum(trainFake) + 1;                 % Ocurrences of each word in fake news (numerator)
     totalWordsFake = sum(wordCountFake);                % Number of total words (denominator)
     
-    probWordFake = wordCountFake/ totalWordsFake;       % Probabilities of each word being in a fake new
-    
-    
+    probWordFake = wordCountFake/ totalWordsFake;       % Probabilities of each word being in a fake new    
+        
+    new = full_news;                         % Extract current new
+
+    % Classify the new
+    class = classifyNew(new, uniqueWords, probWordReal, probWordFake, probFake, probReal);
+
+    fprintf(class)
+    fprint('\n')
+
 end
 
 %% Finalização do Programa
